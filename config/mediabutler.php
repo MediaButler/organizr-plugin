91ce0bed2d254fe643a2be88e18b2a27347cf688
Chris <23177238+vertig0ne@users.noreply.github.com>
2018-02-14 21:03:49 +0000

    Create mediabutler.php

---
+++ config/mediabutler.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'MB-useInternalUrl' => false,
+    'MB-botPrefix' => '!',
+    'MB-sonarrRoot' => '/mnt/media/TV',
+    'MB-sonarrProfile' => 'HD-720p',
+    'MB-radarrRoot' => '/mnt/media/FILM',
+    'MB-radarrProfile' => 'HD-720p',
+    'MB-lidarrRoot' => '/mnt/media/MUSiC',
+    'MB-lidarrProfile' => 'Lossless',
+    'MB-syncLoungeServerUrl' => 'https://eu1.synclounge.tv',
+    'MB-syncLoungeAppUrl' => 'http://app.synclounge.tv/slweb',
+    'MB-tautulliToken' => ''
+);
