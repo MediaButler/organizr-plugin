58228502236a0a526f1c2d5eb22d69e942c00514
Vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-09-01 05:17:09 +0100

    further updates

--- config/mediabutler.php
+++ config/mediabutler.php
@@ -1,14 +1,6 @@
 <?php
 return array(
-    'MB-useInternalUrl' => false,
-    'MB-botPrefix' => '!',
-    'MB-sonarrRoot' => 'Select...',
-    'MB-sonarrProfile' => 'Select...',
-    'MB-radarrRoot' => 'Select...',
-    'MB-radarrProfile' => 'Select...',
-    'MB-lidarrRoot' => 'Select...',
-    'MB-lidarrProfile' => 'Select...',
-    'MB-syncLoungeServerUrl' => 'https://eu1.synclounge.tv',
-    'MB-syncLoungeAppUrl' => 'http://app.synclounge.tv/slweb',
-    'MB-tautulliToken' => ''
+    'MB-enabled' => false,
+    'MB-API-URL' => '!',
+    'MB-API-TOKEN' => 'Select...',
 );
--- js/mediabutler.js
+++ js/mediabutler.js
@@ -1,4 +1,5 @@
 var initialData = false;
+const toTitleCase = (phrase) => { return phrase.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '); };
 $(document).on('click', '#MB-settings-button', function () {
     var post = {
         plugin: 'MB/settings/get',
@@ -9,20 +10,104 @@ $(document).on('click', '#MB-settings-button', function () {
         initialData = response.data;
         $('#MB-settings-items').html(buildFormGroup(initialData));
         $('.MB-TEST-CONN').click(function () {
+            message('MediaButler', ' Grabbing Settings', activeInfo.settings.notifications.position, '#FFF', 'info', '5000');
             var url = $("input[name=MB-API-URL]").val();
             var token = $("input[name=MB-API-TOKEN]").val();
             if (url == undefined && token == undefined) return;
             var newData = [];
-            mbApi(url, token, 'version').then((versionResult) => {
-                for (let i = 0; i < versionResult.plugins.length; i++) {
-                    newData.push(_mbMakeArray(url, token, versionResult.plugins[i]));
-                }
-                var a = buildFormGroup(initialData);
-                var b = _MBbuildFormGroup(newData);
-                console.log(b);
-                $('#MB-settings-items').html(a);
+            var pluginNames = [];
+            var schema = {};
+            mediaAPI('g', url + '/version', token).success(function (data) {
+                $.each(data['plugins'], function (i, v) {
+                    var pluginArray = [];
+                    mediaAPI('g', url + '/configure/' + v, token).success(function (config) {
+                        const name = v;
+                        if (config['schema'].length !== 0) {
+                            // log schema here
+                            schema[name] = config['schema'];
+                            $.each(config['schema'], function (i, v) {
+                                var endType = '';
+                                switch (v.type) {
+                                    case 'url':
+                                        endType = 'input';
+                                        break;
+                                    case 'secure-string':
+                                        endType = 'password-alt';
+                                        break;
+                                    case 'string':
+                                        endType = 'input';
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                if (endType) {
+                                    var valueToPush = {
+                                        "type": endType,
+                                        "name": 'MB-TEMP-' + name + '-' + v.name,
+                                        "value": eval(`config.settings.${v.name}`),
+                                        "label": toTitleCase(v.name)
+                                    };
+                                    pluginArray.push(valueToPush);
+                                } else {
+                                    console.log(`no end type found for ${v.name} - should have found ${v.type}`);
+                                    console.log(v);
+                                }
+                            });
+                            // Add buttons to pluginArray here
+                            var testButton = {
+                                'type': 'button',
+                                'class': `MB-TEMP-${name}-TESTBUTTON`,
+                                'text': 'Test Connection',
+                                'label': 'Test Connection',
+                                'icon': 'fa fa-check',
+                            };
+                            var saveButton = {
+                                'type': 'button',
+                                'class': `MB-TEMP-${name}-SAVEBUTTON`,
+                                'text': 'Save Settings',
+                                'label': 'Save Settings',
+                                'icon': 'fa fa-save',
+                            };
+                            pluginArray.push(testButton);
+                            pluginArray.push(saveButton);
+                            pluginNames.push(name);
+                            initialData[toTitleCase(v)] = pluginArray;
+                        }
+                    }).fail(function (xhr) {
+                        console.error("MB Function: API Connection Failed");
+                    });
+                });
+                $('#MB-settings-items').html(buildFormGroup(initialData));
+                $.each(pluginNames, function (i, v) {
+                    const name = v;
+                    $('.MB-TEMP-' + v + '-TESTBUTTON').click(function () {
+                        const data = {};
+                        $.each(schema[name], function (i, v2) {
+                            data[v2.name] = $("[name='MB-TEMP-" + name + "-" + v2.name + "']").val();
+                        });
+                        mediaAPI('put', url + '/configure/' + name, token, data).success(function (data) {
+                            if (data.message == 'success') message('MediaButler', `Settings for ${toTitleCase(name)} PASSED`, activeInfo.settings.notifications.position, '#FFF', 'info', '5000');
+                            else console.log(data);
+                        }).fail(function(d) {
+                            if (d.response == 'Unauthorized') message('MediaButler', `ERRPR: Unauthorized`, activeInfo.settings.notifications.position, '#FFF', 'error', '5000');
+                            else message('MediaButler', d.responseJSON.message, activeInfo.settings.notifications.position, '#FFF', 'error', '5000');
+                        });
+                    });
+                    $(document).on('click', '.MB-TEMP-' + v + '-SAVEBUTTON', function () {
+                        const data = {};
+                        $.each(schema[name], function (i, v2) {
+                            data[v2.name] = $("[name='MB-TEMP-" + name + "-" + v2.name + "']").val();
+                        });
+                        mediaAPI('p', url + '/configure/' + name, token, data).success(function (data) {
+                            if (data.message == 'success') message('MediaButler', `Settings for ${toTitleCase(name)} SAVED`, activeInfo.settings.notifications.position, '#FFF', 'success', '5000');
+                            else console.log(data);
+                        }).fail(function(d) {
+                            if (d.response == 'Unauthorized') message('MediaButler', `ERRPR: Unauthorized`, activeInfo.settings.notifications.position, '#FFF', 'error', '5000');
+                            else message('MediaButler', d.responseJSON.message, activeInfo.settings.notifications.position, '#FFF', 'error', '5000');
+                        });
+                    });
+                });
             });
-            // Connect to MB API
         });
     }).fail(function (xhr) {
         console.error("Organizr Function: API Connection Failed");
@@ -30,173 +115,46 @@ $(document).on('click', '#MB-settings-button', function () {
     ajaxloader();
 });
 
-function _mbMakeArray(url, token, plugin) {
-    const data = [];
-    mbApi(url, token, `configure/${plugin}`).then((config) => {
-        const name = plugin;
-        for (let a = 0; a < config.schema.length; a++) {
-            var endType;
-            switch (config.schema[a].type) {
-                case 'url':
-                    endType = 'input';
-                    break;
-                case 'secure-string':
-                    endType = 'password-alt';
-                    break;
-                case 'string':
-                    endType = 'input';
-                    break;
-                default:
-                    break;
-            }
-            if (endType) {
-                data.push({ type: endType, name: `MB-TEMP-${plugin}-${config.schema[a].name}`, value: eval(`config.settings.${config.schema[a].name}`), label: config.schema[a].name });
-            } else {
-                console.log(`no end type found for ${config.schema[a].name} - should have found ${config.schema[a].type}`);
-                console.log(config.schema[a]);
-            }
-        }
-    });
-    return data;
-}
-
-
-function mbApi(url, token, endpoint, cb) {
-    var t = $.ajax({
-        url: `${url}${endpoint}`,
-        data: {},
-        headers: { Authorization: `Bearer ${token}` },
-    });
-    return $.when(t);
-}
-
-function _MBbuildFormGroup(array) {
-    var mainCount = 0;
-    var group = '<div class="tab-content">';
-    var uList = '<ul class="nav customtab nav-tabs nav-low-margin" role="tablist">';
-    $.each(array, function (i, v) {
-        mainCount++;
-        var count = 0;
-        var total = v.length;
-        var active = (mainCount == 1) ? 'active' : '';
-        var customID = createRandomString(10);
-        if (i == 'custom') {
-            group += v;
-        } else {
-            uList += `<li role="presentation" class="` + active + `"><a href="#` + customID + cleanClass(`_${i}`) + `" aria-controls="` + i + `" role="tab" data-toggle="tab" aria-expanded="false"><span> ` + i + `</span></a></li>`;
-            group += `
-				<!-- FORM GROUP -->
-				<div role="tabpanel" class="tab-pane fade in `+ active + `" id="` + customID + cleanClass(`_${i}`) + `">
-            `;
-            $.each({v}, function (a, b) {
-                console.log(a);
-                console.log(b);
-                console.log(v.v.length);
-                console.log('hello');
-                b.forEach(function (k) {
-                    console.log(k);
-                    console.log(buildFormItem(k));
-                    var override = '6';
-                    if (typeof k.override !== 'undefined') {
-                        override = k.override;
-                    }
-                    count++;
-                    if (count % 2 !== 0) {
-                        group += '<div class="row start">';
-                    }
-                    var helpID = '#help-info-' + k.name;
-                    var helpTip = (k.help) ? '<sup><a class="help-tip" data-toggle="collapse" href="' + helpID + '" aria-expanded="true"><i class="m-l-5 fa fa-question-circle text-info" title="Help" data-toggle="tooltip"></i></a></sup>' : '';
-                    group += `
-					<!-- INPUT BOX -->
-					<div class="col-md-`+ override + ` p-b-10">
-						<div class="form-group">
-							<label class="control-label col-md-12"><span lang="en">`+ k.label + `</span>` + helpTip + `</label>
-							<div class="col-md-12">
-								`+ _mbBuildFormItem(k) + `
-							</div>
-						</div>
-					</div>
-					<!--/ INPUT BOX -->
-				`;
-                    if (count % 2 == 0 || count == total) {
-                        group += '</div><!--end-->';
-                    }
-                });
-                group += '</div>';
+function mediaAPI(type, path, token, data = null) {
+    console.log('MB API: Calling API: ' + path);
+    switch (type) {
+        case 'get':
+        case 'GET':
+        case 'g':
+            return $.ajax({
+                url: path,
+                method: "GET",
+                async: false,
+                beforeSend: function (request) {
+                    request.setRequestHeader("Authorization", "Bearer " + token);
+                },
+                timeout: 10000,
             });
-        }
-    });
-    return uList + '</ul>' + group;
-}
-
-function _mbBuildFormItem(item) {
-    console.log(item);
-    var placeholder = (item.placeholder) ? ' placeholder="' + item.placeholder + '"' : '';
-    var id = (item.id) ? ' id="' + item.id + '"' : '';
-    var type = (item.type) ? ' data-type="' + item.type + '"' : '';
-    var value = (item.value) ? ' value="' + item.value + '"' : '';
-    var textarea = (item.value) ? item.value : '';
-    var name = (item.name) ? ' name="' + item.name + '"' : '';
-    var extraClass = (item.class) ? ' ' + item.class : '';
-    var icon = (item.icon) ? ' ' + item.icon : '';
-    var text = (item.text) ? ' ' + item.text : '';
-    var attr = (item.attr) ? ' ' + item.attr : '';
-    var disabled = (item.disabled) ? ' disabled' : '';
-    var href = (item.href) ? ' href="' + item.href + '"' : '';
-    var pwd1 = createRandomString(6);
-    var pwd2 = createRandomString(6);
-    var pwd3 = createRandomString(6);
-    var helpInfo = (item.help) ? '<div class="collapse" id="help-info-' + item.name + '"><blockquote>' + item.help + '</blockquote></div>' : '';
-    var smallLabel = (item.smallLabel) ? '<label><span lang="en">' + item.smallLabel + '</span>`+helpTip+`</label>' + helpInfo : '' + helpInfo;
-    var pwgMgr = `
-	<input name="disable-pwd-mgr-`+ pwd1 + `" type="password" id="disable-pwd-mgr-` + pwd1 + `" style="display: none;" value="disable-pwd-mgr-` + pwd1 + `" />
-	<input name="disable-pwd-mgr-`+ pwd2 + `" type="password" id="disable-pwd-mgr-` + pwd2 + `" style="display: none;" value="disable-pwd-mgr-` + pwd2 + `" />
-	<input name="disable-pwd-mgr-`+ pwd3 + `" type="password" id="disable-pwd-mgr-` + pwd3 + `" style="display: none;" value="disable-pwd-mgr-` + pwd3 + `" />
-	`;
-    //+tof(item.value,'c')+`
-    switch (item.type) {
-        case 'input':
-        case 'text':
-            return smallLabel + '<input data-changed="false" lang="en" type="text" class="form-control' + extraClass + '"' + placeholder + value + id + name + disabled + type + attr + ' autocomplete="new-password" />';
             break;
-        case 'number':
-            return smallLabel + '<input data-changed="false" lang="en" type="number" class="form-control' + extraClass + '"' + placeholder + value + id + name + disabled + type + attr + ' autocomplete="new-password" />';
-            break;
-        case 'textbox':
-            return smallLabel + '<textarea data-changed="false" class="form-control' + extraClass + '"' + placeholder + id + name + disabled + type + attr + ' autocomplete="new-password">' + textarea + '</textarea>';
-            break;
-        case 'password':
-            return smallLabel + pwgMgr + '<input data-changed="false" lang="en" type="password" class="form-control' + extraClass + '"' + placeholder + value + id + name + disabled + type + attr + ' autocomplete="new-password" />';
-            break;
-        case 'password-alt':
-            return smallLabel + '<div class="input-group">' + pwgMgr + '<input data-changed="false" lang="en" type="password" class="password-alt form-control' + extraClass + '"' + placeholder + value + id + name + disabled + type + attr + ' autocomplete="new-password" /><span class="input-group-btn"> <button class="btn btn-default showPassword" type="button"><i class="fa fa-eye passwordToggle"></i></button></span></div>';
-            break;
-        case 'hidden':
-            return '<input data-changed="false" lang="en" type="hidden" class="form-control' + extraClass + '"' + placeholder + value + id + name + disabled + type + attr + ' />';
-            break;
-        case 'select':
-            return smallLabel + '<select class="form-control' + extraClass + '"' + placeholder + value + id + name + disabled + type + attr + '>' + selectOptions(item.options, item.value) + '</select>';
-            break;
-        case 'select2':
-            return smallLabel + '<select class="m-b-10 ' + extraClass + '"' + placeholder + value + id + name + disabled + type + attr + ' multiple="multiple" data-placeholder="Choose">' + selectOptions(item.options, item.value) + '</select>';
-            break;
-        case 'switch':
-        case 'checkbox':
-            return smallLabel + '<input data-changed="false" type="checkbox" class="js-switch' + extraClass + '" data-size="small" data-color="#99d683" data-secondary-color="#f96262"' + name + value + tof(item.value, 'c') + id + disabled + type + attr + ' /><input data-changed="false" type="hidden"' + name + 'value="false">';
-            break;
-        case 'button':
-            return smallLabel + '<button class="btn btn-sm btn-success btn-rounded waves-effect waves-light b-none' + extraClass + '" ' + href + attr + 'type="button"><span class="btn-label"><i class="' + icon + '"></i></span><span lang="en">' + text + '</span></button>';
-            break;
-        case 'blank':
-            return '';
-            break;
-        case 'accordion':
-            return '<div class="panel-group' + extraClass + '"' + placeholder + value + id + name + disabled + type + attr + '  aria-multiselectable="true" role="tablist">' + accordionOptions(item.options, item.id) + '</div>';
-            break;
-        case 'html':
-            return item.html;
-            break;
+        case 'post':
+        case 'POST':
+        case 'p':
+            return $.ajax({
+                url: path,
+                method: "POST",
+                async: false,
+                beforeSend: function (request) {
+                    request.setRequestHeader("Authorization", "Bearer " + token);
+                },
+                data
+            });
+        case 'put':
+        case 'PUT':
+            return $.ajax({
+                url: path,
+                method: "PUT",
+                async: false,
+                beforeSend: function (request) {
+                    request.setRequestHeader("Authorization", "Bearer " + token);
+                },
+                data
+            });
         default:
-            return false;
+            console.warn('MB API: Method Not Supported');
     }
 }
